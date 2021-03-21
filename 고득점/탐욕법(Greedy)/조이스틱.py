30352bd18cedef2a52803dba6f7291d119327a7e
hyein99 <hyeinnn999@gmail.com>
2021-03-21 19:19:17 +0900

    0321

--- 고득점/탐욕법(Greedy)/조이스틱.py
+++ 고득점/탐욕법(Greedy)/조이스틱.py
@@ -1,20 +1,71 @@
+def choosedir(idx, dlist):
+    right, left = 1, 1
+    while dlist[idx + right] == 0:
+        right += 1
+    while dlist[idx - left] == 0:
+        # print(idx-left)
+        left += 1
+
+    if left < right:
+        move = left
+        new_idx = idx - move
+    else:
+        move = right
+        new_idx = idx + move
+
+    return new_idx, move
+
+
 def solution(name):
-    def move_joy(x):
-        return min(ord(x) - ord('A'), ord('Z') - ord(x) + 1)
+    answer = 0
+    # step 1: name[i]를 A로 바꾸기 위한 최소 이동거리
+    dlist = [min(ord(n) - ord('A'), ord('Z') - ord(n) + 1) for n in name]
 
-    result = 0
-    # 첫번째 글자
-    result += move_joy(name[0])
-    if len(name) < 2:
-        return result
+    # step 2
+    idx = 0
+    cnt = len(dlist) - dlist.count(0)  # 바꿔야할 문자수
 
-    # 건너 뛸 A
-    i = 1
-    while name[i] == 'A':
-        i += 1
+    while True:
+        answer += dlist[idx]  # 바꿈
+        if dlist[idx] != 0:
+            cnt -= 1
+        dlist[idx] = 0  # 이동 완료
 
-    while i < len(name):
-        result += move_joy(name[i]) + 1
-        i += 1
+        # 종료 조건
+        if cnt <= 0:
+            break
 
-    return result
+        # 오? 왼?
+        idx, move = choosedir(idx, dlist)  # 이동한 위치, 오 왼 이동 거리 반환
+        answer += move
+
+    return answer
+
+
+# JBAAAAAC
+
+
+# def solution(name):
+#     def move_joy(x):
+#         return min(ord(x) - ord('A'), ord('Z') - ord(x) + 1)
+#
+#     result = 0
+#     # 첫번째 글자
+#     result += move_joy(name[0])
+#     if len(name) < 2:
+#         return result
+#
+#     # 건너 뛸 A
+#     i = 1
+#     while name[i] == 'A':
+#         i += 1
+#
+#     while i < len(name):
+#         result += move_joy(name[i]) + 1
+#         i += 1
+#
+#     return result
+
+
+print(solution('JAM'))
+print(solution('ABAAAAAAAAABB'))
--- 고득점/탐욕법(Greedy)/체육복.py
+++ 고득점/탐욕법(Greedy)/체육복.py
@@ -1,35 +1,41 @@
 def solution(n, lost, reserve):
+    # lost이면서 reserve인 학생 제거
     L = set(lost)-set(reserve)
     R = set(reserve)-set(lost)
 
-    for r in R:
+    for r in R:     # 여벌 체육복을 가져온 학생
         if r-1 in L:
             L.remove(r-1)
         elif r+1 in L:
             L.remove(r+1)
 
+    # 체육복을 갖게 된 학생 수 리턴
     return n - len(L)
 
 
-# def solution(n, lost, reserve):
-#     L = set(lost)-set(reserve)
-#     R = set(reserve)-set(lost)
-#
-#     cnt, i, j = 0, 0, 0
-#     while i < len(L) and j < len(R):
-#         if R[j] - 1 > L[i]:
-#             cnt += 1
-#             i += 1
-#         elif R[j] + 1 < L[i]:
-#             j += 1
-#         else:
-#             i += 1
-#             j += 1
-#     cnt += len(L) - i
-#
-#     return n - cnt
+def solution2(n, lost, reserve):
+    L = sorted(list(set(lost)-set(reserve)))
+    R = sorted(list(set(reserve)-set(lost)))
+
+    cnt, i, j = 0, 0, 0   # 체육복 못빌린 학생수, lost index, reserve index
+    while i < len(L) and j < len(R):
+        # Reserve를 기준으로 Lost가 체육복을 빌릴 수 있는지 판단
+        # case 1: 빌려줄 수 있는 범위 최소값보다 작은 학생 있으면 못빌리는 학생으로 분류
+        if R[j] - 1 > L[i]:
+            cnt += 1
+            i += 1
+        # case 2: 빌려줄 수 있는 범위보다 크면 다음 Reserve 학생으로 넘어감
+        elif R[j] + 1 < L[i]:
+            j += 1
+        # case 3: R이 L에게 빌려줄 수 있는 경우
+        else:
+            i += 1
+            j += 1
+    cnt += len(L) - i # 남은 체육복 못 빌린 애들 추가 해줌
+
+    return n - cnt
 
 # print(solution(n=5, lost=[2,4], reserve=[1,3,5]))
 # print(solution(n=5, lost=[2,4], reserve=[3]))
-print(solution(n=7, lost=[2,3,4], reserve=[1,2,3,6]))
-print(solution(n=5, lost=[1,2,3], reserve=[2,3,4]))
+# print(solution2(n=7, lost=[1,2,3,4,5,6,7], reserve=[1,2,3]))
+print(solution2(n=5, lost=[1,2,4,5], reserve=[2,3,4]))
